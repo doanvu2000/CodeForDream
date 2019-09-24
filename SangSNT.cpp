4f29a89051a836a569cfc36fc72570b687cf63ab
Vũ Văn Doan <55150896+vuvandoancnttk13haui@users.noreply.github.com>
2019-09-24 08:59:28 +0700

    Update SangSNT.cpp

--- SangSNT.cpp
+++ SangSNT.cpp
@@ -7,12 +7,9 @@ int main(){
 	int n = 1000000;
 //	cout<<"nhap n: ";cin>>n;
 	vector<bool>snt(n+1,true);
-	snt[1]=false;
-	for(int i=2;i<=n;i++)
-		snt[i] = true;
-	int i= 2 ;
-	while (i<=trunc(sqrt(n))){
-		while(snt[i]==0)
+	int i = 2 ;
+	while (i*i<=n){ 
+		while(!snt[i])
 			i++;
 		for(int j=2;j<=n/i;j++)
 			snt[i*j] = false;
